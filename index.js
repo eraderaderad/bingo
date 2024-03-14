26e373439f9ebeef974dab11320a7e177523e131
eraderaderad <gerardcarlos212712@gmail.com>
2024-03-14 15:37:38 +0800

    index

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-console.log("Hello World.");
+console.log("me myself and i");
 
-function generateRandomNumber() 
-{
-    let randomNum = Math.floor(Math.random() * 10) + 4;
+generateRandomNumber(30);
 
-if(randomNum >= 5)
-{
-    console.log(`Random Number is >= to 5`);
-} else {
-    console.log(`Random Number is <= to 5`);
-}
+function generateRandomNumber(max) 
+    {
+        let randomNum = Math.floor(Math.random() * max) + 1;
 
-return console.log(randomNum);
+        if(randomNum <= 15)
+        {
+            return console.log(`Random Number ${randomNum} is in B. 1 - 15`)
+        } 
 
-}
+        else if (randomNum >= 16 && randomNum <= 30) {
+            return console.log(`Random Number ${randomNum} is I. 16 - 30`)
+        }
+
+        else if (randomNum >= 31 && randomNum <= 45) {
+            return console.log(`Random Number ${randomNum} is N. 31 - 46`)
+        }
+
+        else if (randomNum >= 46 && randomNum <= 60) {
+            return console.log(`Random Number ${randomNum} is G. 46 - 61`)
+        }
+        
+        else if (randomNum >= 61 && randomNum <= 75) {
+            return console.log(`Random Number ${randomNum} is O. 61 - 75`)
+        }
+
+        else {
+            return console.log(`Random Number is invalid.`)
+        }
+    }
+
+        // switch(randomNum){
+        //     case 1:{
+        //         console.log(`Random number is in B.`);
+        //         break;
+        //     }
+        //     case 16:{
+        //         console.log(`Random number is in I.`);
+        //         break;
+        //     }
+        //     case 31:{
+        //         console.log(`Random number is in N.`);
+        //         break;
+        //     }
+        //     case 46:{
+        //         console.log(`Random number is in G.`);
+        //         break;
+        //     }
+        //     case 61:{
+        //         console.log(`Random number is in O.`);
+        //         break;
+        //     }
+        // }
